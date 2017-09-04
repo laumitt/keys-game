fcb57e21ecc2e3c78e23d9825b1f6c81b0ab6772
Laurel Mitton <laumitt@nhs11250-11-laumitt.attlocal.net>
2017-09-03 22:04:50 -0700

    Starting a new project

---
+++ keyspuzzle.py
@@ -0,0 +1,25 @@
+RedMatrix = [[1, 3],
+             [5, 2],
+             [5, 1]]
+OrangeMatrix = [[2, 4],
+                [3, 1],
+                [2, 1]]
+YellowMatrix = [[3, 1],
+                [4, 4],
+                [4, 5]]
+LimeMatrix = [[4, 2],
+              [5, 3],
+              [5, 4]]
+GreenMatrix = [[3, 5],
+               [1, 2],
+               [1, 1]]
+BlueMatrix = [[5, 3],
+              [1, 4],
+              [1, 5]]
+PurpleMatrix = [[4, 4],
+                [4, 2],
+                [4, 1]]
+WhiteMatrix = [1, 5]
+BrownMatrix = [5, 5]
+BlackMatrix = [5, 1]
+PinkMatrix = [1,1]
