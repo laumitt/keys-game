c3028a8e0f8213f812992fae4e3f25536da994c3
laumitt <laumitt@nuevaschool.org>
2017-10-05 15:30:43 -0700

    updated matrix

--- keys3.py
+++ keys3.py
@@ -3,32 +3,32 @@ Matrix = [["0010", "0110", "0101", "0011", "0010"],
           ["1110", "0101", "0011", "0100", "1011"],
           ["1010", "0100", "1101", "0111", "1011"],
           ["1000", "0100", "0101", "1001", "1000"]]
-# a  5 by 5 matrix with each row numbered 1-5
+# a  5 by 5 matrix, 0 - 4 by 0 - 4
 # defines each position's possible movement directions
 # 0 means can't go, 1 means can go, NESW
 
 # the room 1,1 is 11, the room 1,2 is 12 and to the right of 11, etc
-Crystals = {"pink" : "11",
-            "white" : "15",
-            "brown" : "55",
-            "black" : "51"}
+Crystals = {"pink" : "04",
+            "white" : "00",
+            "brown" : "40",
+            "black" : "44"}
             # where the crystals are
-Keys = {"red" : "13",
-        "orange" : "24",
-        "yellow" : "31",
-        "lime" : "42",
-        "green" : "35",
-        "teal" : "22",
-        "blue" : "53",
-        "purple" : "44"}
+Keys = {"red" : "02",
+        "orange" : "11",
+        "yellow" : "24",
+        "lime" : "33",
+        "green" : "20",
+        "teal" : "13",
+        "blue" : "42",
+        "purple" : "31"}
         # where the keys are
 
 class Room:
     def __init__(self):
-        self.x = 3
-        self.y = 3
+        self.x = 2
+        self.y = 2
         self.position = Matrix[self.x][self.y]
-        # start at 3, 3
+        # start at 2, 2
     def check(self):
         if self.position[0] == "1":
             print("You can GO NORTH.")
@@ -69,8 +69,9 @@ class Room:
         location = str(self.x) + str(self.y)
         if location in Keys:
             print("You collected the " + Keys(str(self.x) + str(self.y)) + " key!")
+
 # class Wall:
-    # something about the doors and needing keys
+# if the movement is going through a wall stop the movement
 
 # class Maze (Wall, Room):
 # if the movement is going through a door, check for the key, then move
@@ -87,7 +88,7 @@ if __name__ == "__main__":
         maze.movement(move)
         maze.checkCrystal()
         maze.checkKeys()
-        if maze.x == 2 and maze.y == 1 :
+        if maze.x == 1 and maze.y == 4 :
         # if you find the end
             print("You found the end!")
             # you won
