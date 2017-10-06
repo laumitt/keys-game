03f57ee6ed21d0d95d497db4bdae6d6d47e55062
laumitt <laumitt@nuevaschool.org>
2017-10-06 14:15:45 -0700

    new way of storing and checking for keys and crystals

--- keys3.py
+++ keys3.py
@@ -1,26 +1,26 @@
-Matrix = [["0010", "0110", "0101", "0011", "0010"],
-          ["1010", "1000", "0100", "1101", "1011"],
-          ["1110", "0101", "0011", "0100", "1011"],
-          ["1010", "0100", "1101", "0111", "1011"],
-          ["1000", "0100", "0101", "1001", "1000"]]
+Matrix = [["0010c", "0110n", "0101y", "0011n", "0010c"],
+          ["1010n", "1000o", "0100n", "1101ln", "1011n"],
+          ["1110r", "0101", "0011", "0100", "1011g"],
+          ["1010n", "0100p", "1101n", "0111t", "1011n"],
+          ["1000c", "0100n", "0101b", "1001n", "1000c"]]
 # a  5 by 5 matrix, 0 - 4 by 0 - 4
 # defines each position's possible movement directions
 # 0 means can't go, 1 means can go, NESW
 
 # the room 1,1 is 11, the room 1,2 is 12 and to the right of 11, etc
-Crystals = {"pink" : "04",
-            "white" : "00",
-            "brown" : "40",
-            "black" : "44"}
+#Crystals = {"pink" : "04",
+#            "white" : "00",
+#            "brown" : "40",
+#            "black" : "44"}
             # where the crystals are
-Keys = {"red" : "02",
-        "orange" : "11",
-        "yellow" : "24",
-        "lime" : "33",
-        "green" : "20",
-        "teal" : "13",
-        "blue" : "42",
-        "purple" : "31"}
+# Keys = {"red" : "02",
+    #    "orange" : "11",
+    #    "yellow" : "24",
+    #    "lime" : "33",
+    #    "green" : "20",
+    #    "teal" : "13",
+    #    "blue" : "42",
+    #    "purple" : "31"}
         # where the keys are
 
 class Room:
@@ -28,8 +28,10 @@ class Room:
         self.x = 2
         self.y = 2
         self.position = Matrix[self.x][self.y]
+        self.numcrystals = 0
+        self.inventory = ["inventory"]
         # start at 2, 2
-    def check(self):
+    def checkMoves(self):
         if self.position[0] == "1":
             print("You can GO NORTH.")
         # if the matrix says you can go north, say you can go north
@@ -61,14 +63,50 @@ class Room:
             self.x = self.x - 1
             self.position = Matrix[self.x][self.y]
             print("You are now at " + str(self.x) + ", " + str(self.y))
-    def checkCrystal(self):
-        location = str(self.x) + str(self.y)
-        if location in Crystals:
-            print("You collected the " + Crystals(str(self.x) + str(self.y)) + " crystal!")
-    def checkKeys(self):
-        location = str(self.x) + str(self.y)
-        if location in Keys:
-            print("You collected the " + Keys(str(self.x) + str(self.y)) + " key!")
+class Objects(Room):
+    def checkKey(self):
+        if self.position[4] == "r":
+            print("Red Key found")
+            self.inventory.append["red"]
+        elif self.position[4] == "o":
+            print("Orange Key found")
+            self.inventory.append["orange"]
+        elif self.position[4] == "y":
+            print("Yellow Key found")
+            self.inventory.append["yellow"]
+        elif self.position[4] == "l":
+            print("Lime Key found")
+            self.inventory.append["lime"]
+        elif self.position[4] == "g":
+            print("Green Key found")
+            self.inventory.append["green"]
+        elif self.position[4] == "t":
+            print("Teal Key found")
+            self.inventory.append["teal"]
+        elif self.position[4] == "b":
+            print("Blue Key found")
+            self.inventory.append["blue"]
+        elif self.position[4] == "p":
+            print("Purple Key found")
+            self.inventory.append["purple"]
+        elif self.position[4] == "c" and self.numcrystals == 0:
+            print("Crystal 1 found")
+            self.inventory.append["crystal1"]
+            self.numcrystals = 1
+        elif self.position[4] == "c" and self.numcrystals == 1:
+            print("Crystal 2 found")
+            self.inventory.append["crystal2"]
+            self.numcrystals = 2
+        elif self.position[4] == "c" and self.numcrystals == 2:
+            print("Crystal 3 found")
+            self.inventory.append["crystal3"]
+            self.numcrystals = 3
+        elif self.position[4] == "c" and self.numcrystals == 3:
+            print("Last Crystal found")
+            self.inventory.append["crystal4"]
+            self.numcrystals = 4
+    def printInventory(self):
+        print("Current Inventory: " + self.inventory)
 
 # class Wall:
 # if the movement is going through a wall stop the movement
@@ -78,16 +116,18 @@ class Room:
 
 if __name__ == "__main__":
     maze = Room()
+    items = Objects()
     print("Welcome to The Key Maze. Collect all four crystals and reach 2, 1.")
     print("You are now at " + str(maze.x) + ", " + str(maze.y))
     game = True
     # say where you start
     while game == True :
-        maze.check()
+        maze.checkMoves()
         move = input()
         maze.movement(move)
-        maze.checkCrystal()
-        maze.checkKeys()
+        #items.checkCrystal()
+        items.checkKey()
+        items.printInventory
         if maze.x == 1 and maze.y == 4 :
         # if you find the end
             print("You found the end!")
